9e7b5b6c0dde3994ee6f2c02eb5784947bd3e1ca
Thilina Dilshan <thilinadilshan626@gmail.com>
2026-01-08 20:45:00 +0530

    feat: Fix the home page navigating error

--- Front-end/src/App.tsx
+++ Front-end/src/App.tsx
@@ -18,7 +18,7 @@ export default function App() {
     return (
         <ThemeProvider>
             <MusicProvider>
-                <BrowserRouter>
+                <BrowserRouter basename="/Music-Platform">
                     <ScrollToTop />
                     <MainLayout>
                         <Routes>
